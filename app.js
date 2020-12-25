023e207fc179b7f32e39454ad5562e600741c67a
lqzh <lqzhzc@outlook.com>
2020-12-25 17:39:41 +0800

    ✨feat(Improve the mp3 quality):
    add ffmpeg param to up mp3 quality

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ function amrToMp3(filepath, outputDir = './src/mp3', outputName) {
             return;
         }
         const _outputName = outputName || filename;
-        const cmdStr = `${ffmpegPath} -y -i "${path.normalize(filepath)}" "${path.join(outputDir, _outputName + '.mp3')}"`;
-        console.log('cmdStr', cmdStr);
+        const cmdStr = `${ffmpegPath} -y -i "${path.normalize(filepath)}" -acodec libmp3lame -ar 24000 -vol 500 -ab 128 "${path.join(outputDir, _outputName + '.mp3')}"`;
         exec(cmdStr, (err, stdout, stderr) => {
             if (err) {
                 // console.log('error:' + stderr);
